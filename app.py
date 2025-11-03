aa2465e2547ed7850ed1b463df5dbe96d63a96e3
saharata <87695101+saharata@users.noreply.github.com>
2025-11-03 23:12:02 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from PIL import Image
-from fpdf import FPDF
+from fpdf import FPDF, __version__ as fpdf_version
+st.caption(f"FPDF version: {fpdf_version}")
 import base64
 import datetime
 import os
